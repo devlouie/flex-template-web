73290de8ad41cdfbaa23808ab008b5ff3cf52b35
devlouie <developerlouie@gmail.com>
2019-05-15 21:46:28 +1000

    reverting changes

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,25 +7,25 @@ import IconLogo from './IconLogo';
 import LogoImage from './saunatime-logo.png';
 import css from './Logo.css';
 
-// const Logo = props => {
-//   const { className, format, ...rest } = props;
-//   const mobileClasses = classNames(css.logoMobile, className);
-
-//   if (format === 'desktop') {
-//     return <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />;
-//   }
-
-//   return <IconLogo className={mobileClasses} {...rest} />;
-// };
-
 const Logo = props => {
   const { className, format, ...rest } = props;
   const mobileClasses = classNames(css.logoMobile, className);
+
+  if (format === 'desktop') {
+    return <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />;
+  }
+
+  return <IconLogo className={mobileClasses} {...rest} />;
+};
+
+// const Logo = props => {
+//   const { className, format, ...rest } = props;
+//   const mobileClasses = classNames(css.logoMobile, className);
   
-  return <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />;
+//   return <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />;
 
   
-};
+// };
 
 
 const { oneOf, string } = PropTypes;
