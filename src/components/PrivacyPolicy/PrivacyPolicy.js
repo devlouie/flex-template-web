e07d55e619e6adc364be64a124ade331730c1c58
devlouie <developerlouie@gmail.com>
2019-05-21 11:01:16 +1000

    updated privacy policy to reflect motomo

--- src/components/PrivacyPolicy/PrivacyPolicy.js
+++ src/components/PrivacyPolicy/PrivacyPolicy.js
@@ -12,50 +12,130 @@ const PrivacyPolicy = props => {
   return (
     <div className={classes}>
       <p className={css.lastUpdated}>Last updated: October 30, 2017</p>
-
-      <p>
-        Thank you for using Saunatime! Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    
+      <p> 
+    Motomo is committed to providing quality services to you and this policy outlines our ongoing obligations to you in respect of how we manage your Personal Information.
+We have adopted the Australian Privacy Principles (APPs) contained in the Privacy Act 1988 (Cth) (the Privacy Act). The NPPs govern the way in which we collect, use, disclose, store, secure and dispose of your Personal Information.
       </p>
 
-      <h2>1 Lorem ipsum dolor sit amet</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <p> 
+
+  A copy of the Australian Privacy Principles may be obtained from the website of The Office of the Australian Information Commissioner at www.aoic.gov.au.
       </p>
 
-      <h2>2 Sed ut perspiciatis unde</h2>
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
-        dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-        consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
-        molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-      </p>
+  <h2> 
+    What is Personal Information and why do we collect it?
+    </h2>
+  
+  <p>
+    Personal Information is information or an opinion that identifies an individual. Examples of Personal Information we collect include: names, addresses, email addresses, phone and facsimile numbers.
+This Personal Information is obtained in many ways including email and from third parties. We don’t guarantee website links or policy of authorised third parties.
 
-      <h2>3 At vero eos et accusamus</h2>
-      <p>
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-        voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
-        cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id
-        est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam
-        libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod
-        maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
-        Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
-        et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a
-        sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis
-        doloribus asperiores repellat
-      </p>
+    </p>
+    
+    <p>
+We collect your Personal Information for the primary purpose of providing our services to you, providing information to our clients and marketing. We may also use your Personal Information for secondary purposes closely related to the primary purpose, in circumstances where you would reasonably expect such use or disclosure. You may unsubscribe from our mailing/marketing lists at any time by contacting us in writing.
+</p>
+  
+  <p>
+  When we collect Personal Information we will, where appropriate and where possible, explain to you why we are collecting the information and how we plan to use it.
+  </p>
+  
+  <h2> 
+    Sensitive Information
+    </h2> 
+  
+  <p> 
+      Sensitive information is defined in the Privacy Act to include information or opinion about such things as an individual's racial or ethnic origin, political opinions, membership of a political association, religious or philosophical beliefs, membership of a trade union or other professional body, criminal record or health information.
+</p>
+  <ul> Sensitive information will be used by us only:
+
+        <li> 
+          For the primary purpose for which it was obtained
+        </li>
+            <li> 
+          For a secondary purpose that is directly related to the primary purpose
+        </li>
+        <li> 
+          With your consent; or where required or authorised by law.
+        </li>
+  
+  </ul>
+  
+  <h2> 
+            Third Parties
+</h2>
+  <p> 
+              Where reasonable and practicable to do so, we will collect your Personal Information only from you. However, in some circumstances we may be provided with information by third parties. In such a case we will take reasonable steps to ensure that you are made aware of the information provided to us by the third party.
+</p>
+  
+    <h2> 
+    Disclosure of Personal Information
+    </h2> 
+    <ul> Your Personal Information may be disclosed in a number of circumstances including the following:
+
+        <li> 
+          Third parties where you consent to the use or disclosure; and
+        </li>
+            <li> 
+          Where required or authorised by law.
+        </li>  
+  </ul>
+  
+    <h2> 
+            Security of Personal Information
+</h2>
+  <p> 
+Your Personal Information is stored in a manner that reasonably protects it from misuse and loss and from unauthorized access, modification or disclosure.
+</p>
+
+    <p> 
+When your Personal Information is no longer needed for the purpose for which it was obtained, we will take reasonable steps to destroy or permanently de-identify your Personal Information. However, most of the Personal Information is or will be stored in client files which will be kept by us for a minimum of 7 years.
+
+  </p>
+
+  
+    <h2> 
+            Access to your Personal Information
+</h2>
+  <p> 
+You may access the Personal Information we hold about you and to update and/or correct it, subject to certain exceptions. If you wish to access your Personal Information, please contact us in writing.
+</p>
+
+    <p> 
+Motomo will not charge any fee for your access request, but may charge an administrative fee for providing a copy of your Personal Information.
+</p>
+
+    <p> 
+In order to protect your Personal Information we may require identification from you before releasing the requested information.
+</p>
+
+    
+    <h2> 
+            Maintaining the Quality of your Personal Information
+</h2>
+  <p> 
+It is important to us that your Personal Information is up to date. We  will  take reasonable steps to make sure that your Personal Information is accurate, complete and up-to-date. If you find that the information we have is not up to date or is inaccurate, please advise us as soon as practicable so we can update our records and ensure we can continue to provide quality services to you.
+</p>
+
+      <h2> 
+            Policy Updates
+</h2>
+  <p> 
+This Policy may change from time to time and is available on our website.
+</p>
+
+        <h2> 
+Privacy Policy Complaints and Enquiries
+</h2>
+  <p> 
+This Policy may change from timIf you have any queries or complaints about our Privacy Policy please contact us at:
+150 Elizabeth St, Sydney, 2000
+support@motomo.io
+e to time and is available on our website.
+</p>
+
+  
     </div>
   );
 };
