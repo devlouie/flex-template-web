b7eaf6c8bb02bd8052cff677288812a2e7e18711
devlouie <developerlouie@gmail.com>
2019-06-22 21:39:31 +1000

    Removed links for help and contact temporarily

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -96,16 +96,6 @@ const Footer = props => {
                     <FormattedMessage id="Footer.toFAQPage" />
                   </NamedLink>
                 </li>
-                <li className={css.listItem}>
-                  <NamedLink name="LandingPage" className={css.link}>
-                    <FormattedMessage id="Footer.toHelpPage" />
-                  </NamedLink>
-                </li>
-                <li className={css.listItem}>
-                  <NamedLink name="AboutPage" to={{ hash: '#contact' }} className={css.link}>
-                    <FormattedMessage id="Footer.toContactPage" />
-                  </NamedLink>
-                </li>
               </ul>
             </div>
             <div className={css.searches}>
